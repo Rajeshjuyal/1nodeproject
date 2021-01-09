f8b405faddd3c6450c2d79e0b3826e29f9ebd0bf
Rajesh <juyalrajesh36@gmail.com>
2021-01-09 23:32:26 +0530

    importing passport,JwtStrategy,AuthService on diifernt modules of Api

--- src/assignment/assignemnt.module.ts
+++ src/assignment/assignemnt.module.ts
@@ -4,6 +4,10 @@ import { AssignemntController } from './assignemnt.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AssignmentSchema } from './assignment.model';
 import { TeacherSchema } from '../teacher/teacher.model';
+import { JwtStrategy } from '../utils/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from '../utils/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -11,8 +15,18 @@ import { TeacherSchema } from '../teacher/teacher.model';
       { name: 'Assignment', schema: AssignmentSchema },
       { name: 'Teacher', schema: TeacherSchema },
     ]),
+    JwtModule.register({
+      secret: '12345678923sonali',
+      signOptions: {
+        expiresIn: '4H',
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
   ],
   controllers: [AssignemntController],
-  providers: [AssignemntService],
+  providers: [AssignemntService, AuthService],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AssignemntModule {}
--- src/attendence/attendence.module.ts
+++ src/attendence/attendence.module.ts
@@ -3,14 +3,28 @@ import { AttendenceService } from './attendence.service';
 import { AttendenceController } from './attendence.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AttendanceSchema } from './attendence.modle';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from '../utils/auth.service';
+import { JwtStrategy } from '../utils/jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Attendance', schema: AttendanceSchema },
     ]),
+    JwtModule.register({
+      secret: '12344567810sonali',
+      signOptions: {
+        expiresIn: '4h',
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
   ],
   controllers: [AttendenceController],
-  providers: [AttendenceService],
+  providers: [AttendenceService, AuthService, JwtStrategy],
+  exports: [JwtModule, PassportModule],
 })
 export class AttendenceModule {}
