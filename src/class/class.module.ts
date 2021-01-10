10109863e35f3ce43f508a5417c0d7b378b6c262
Rajesh <juyalrajesh36@gmail.com>
2021-01-10 21:06:13 +0530

    passport module on class

--- src/class/class.module.ts
+++ src/class/class.module.ts
@@ -4,14 +4,27 @@ import { ClassController } from './class.controller';
 import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClassSchema } from './class model';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from '../utils/auth.service';
+import { JwtStrategy } from '../utils/jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Class', schema: ClassSchema }]),
-    
+    JwtModule.register({
+      secret: '1234566789sonali',
+      signOptions: {
+        expiresIn: '4h',
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
   ],
 
   controllers: [ClassController],
-  providers: [ClassService],
+  providers: [ClassService, AuthService, JwtStrategy],
+  exports: [JwtModule, PassportModule],
 })
 export class ClassModule {}
